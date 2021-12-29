782269eb788f7e466a39878148e7584ec26dfaa8
unknown <suman.singh@wsisrdev.com>
2021-12-29 12:41:06 +0530

    code update

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,38 @@ import * as express from "express";
 import * as BodyParser from "body-parser";
 import * as cors from "cors";
 import postRoutes from "./routes/userRoutes";
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+import router from "./routes/userRoutes";
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Backend dev API",
+			version: "1.0.0",
+			description: "A simple Backend Dev API",
+		},
+		servers: [
+			{
+				url: "http://localhost:8080",
+			},
+		],
+	},
+	apis: ["./routes/*.ts"],
+  explorer:true
+};
+
+const specs = swaggerJsDoc(options);
+
+
+
+
 
 createConnection()
   .then(async (connection) => {
     const app = express();
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
     app.use(cors());
     app.use(BodyParser.json());
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,75 @@
 import { Router } from "express";
+const express = require ('express');
 import UserController from "../controllers/UserController";
 
 const router = Router();
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - phone
+ *         - email
+ *         - password
+ * 
+ *       properties:
+ *         id:
+ *           type: number
+ *           description: The auto-generated id of the User
+ *         name:
+ *           type: string
+ *           description: The User name
+ *         phone:
+ *           type: string
+ *           description: The phone number of User
+ *         email:
+ *           type: string
+ *           description: The email address of User 
+ *         passsword:
+ *           type: string
+ *           description: The secret password of User 
+ *       example:
+ *          id:1
+ *          name"summi
+ *          phone:8448131010
+ *          email:summich12@gmail.com
+ *          password:summi12
+ */
+
+/**
+  * @swagger
+  * tags:
+  *   name: User
+  *   description: The User managing API
+  */
+
+ /**
+ * @swagger
+ * /createUser:
+ *   post:
+ *     summary: Create a new User
+ *     tags: {User}
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The User was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
 
 router.post("/createUser", UserController.createUser);
 router.get("/getUsers", UserController.getUsers);
